989e9fd82fd02802e1cda34d7a8245f8397687ff
Ethun Pandey <139181274+ethun009@users.noreply.github.com>
2024-07-15 14:58:51 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
 const menuIcon =document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
 const root =document.documentElement
-const theme = document.querySelector('#theme')
+const themebtn = document.querySelector('#theme') ;
+const theme = document.querySelector('.theme') ;
+
+
+const dark = document.querySelector('.dark') ;
+const white =document.querySelector('.light') ;
+const pink =document.querySelector('.pink') ;
+const purple =document.querySelector('.purple') ;
+const blue =document.querySelector('.blue') ;
+const pinkwhite =document.querySelector('.pink-white') ;
+const purplewhite =document.querySelector('.purple-white');
+const bluewhite =document.querySelector('.blue-white') ;
 
 menuIcon.onclick = () => {
   menuIcon.classList.toggle('bx-x');
   navbar.classList.toggle('active')
 }
 
-theme.onclick =() => {
-  root.classList.toggle('white')
+themebtn.onclick =() => {
+  theme.classList.toggle('hide')
+}
+
+dark.onclick = () => {
+  root.removeAttribute("class")
+}
+white.onclick = () => {
+  root.removeAttribute("class")
+  root.classList.add('white')
+}
+pink.onclick = () => {
+  root.removeAttribute("class")
+  root.classList.add('pink')
+}
+purple.onclick = () => {
+  root.removeAttribute("class")
+  root.classList.add('purple')
+}
+blue.onclick = () => {
+  root.removeAttribute("class")
+  root.classList.add('blue')
+}
+bluewhite.onclick = () => {
+  root.removeAttribute("class")
+  root.classList.add('blue-white')
+}
+pinkwhite.onclick = () => {
+  root.removeAttribute("class")
+  root.classList.add('pink-white')
+}
+
+purplewhite.onclick = () => {
+  root.removeAttribute("class")
+  root.classList.add('purple-white')
 }
